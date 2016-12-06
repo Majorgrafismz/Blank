ee144424e3528cd3cdbcf73e02cffa3f568f334c
Laurent Abemango <abemango.laurent@gmail.com>
2016-12-06 18:51:16 +0100

    new exos

---
+++ JS/exo1.js
@@ -0,0 +1,11 @@
+/*declaration variable*/
+var weed = "i LOVE WEED ";
+var sta = 29+7;
+var sto = 76;
+var res = sta/sto;
+
+/*affichage variable*/
+console.log(weed);
+console.log(sta);
+console.log(res);
+console.log(weed+"\"SO MUCH !\"")
